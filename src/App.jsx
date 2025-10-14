5d2e5ca292b4728efd32da9fecceb9f634292478
yeeun <jovm268@naver.com>
2025-10-14 18:15:15 +0900

    feat : 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/quiz/:nickname" element={<Quiz></Quiz>}></Route>
+        <Route path="results/:nickname" element={<Results></Results>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
