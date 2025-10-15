6af80d7621673cf8939f3e78af3f41586028ca05
yeeun <jovm268@naver.com>
2025-10-15 23:43:22 +0900

    refactor : form 이벤트 연결, 접근성 추가 (label)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,26 +5,30 @@ export default function Home() {
   console.log(`입력된 닉네임 ${nickName}`);
   const navigate = useNavigate();
 
-  //시작하기 버튼 분기처리
-  const handleStartBtn = () => {
-    //- 닉네임이 입력되지 않은 경우: 닉네임을 입력해주세요.라는 경고창(alert) 을 띄웁니다.
-    if (!nickName) {
+  //닉네임 입력 버튼 분기처리
+  const handleStartBtn = (e) => {
+    e.preventDefault();
+    if (!nickName.trim()) {
       alert("닉네임을 입력해주세요 ‼");
+      return;
     } else {
-      //- 닉네임이 입력된 경우: /quiz/(입력된 닉네임) 경로로 페이지를 라우팅
       navigate(`/quiz/${nickName}`);
     }
   };
   return (
     <main>
-      <form action={handleStartBtn}>
+      {/* onSubmit={handleStartBtn} 형태로 이벤트를 연결하는 게 React 방식 */}
+      <form onSubmit={handleStartBtn}>
         <h1>UQuiz?</h1>
+        <label htmlFor="nickName-input">닉네임 : </label>
         <input
+          id="nickName-input"
           name="nickName"
           type="text"
           placeholder="닉네임을 입력하세요."
           value={nickName}
           onChange={(e) => setNickName(e.target.value)}
+          required
         />
         <button type="submit">시작하기</button>
       </form>
